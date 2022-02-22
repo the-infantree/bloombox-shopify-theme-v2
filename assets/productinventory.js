86a6cd77c925a39141dbe18e87d9f117015dfb38
Eric Clark <eclarrrk@users.noreply.github.com>
2022-02-22 16:55:58 -0500

    Another try at PDP variant qty

--- assets/productinventory.js
+++ assets/productinventory.js
@@ -85,8 +85,8 @@ function checkavailablity(variation_value)
                 if(locationvar[locvariable]['qty'] < 10){document.querySelectorAll(".qtyerrorapi")[0].innerHTML=`<span class="qtynotice">Only ${locationvar[locvariable]['qty']} Left.</span>`;}
               	document.querySelectorAll(".product-form__row")[0].style.display = "block";
                 document.querySelectorAll(".alert--coming-soon")[0].style.display = "none";
-                var variantQty = document.createTextNode(locationvar[locvariable]["qty"]);
-                document.querySelector("#product__variant-qty span").appendChild(variantQty);
+                var variantQty = document.createTextNode("This just got added");
+                document.querySelector("#product__variant-qty").innerHTML = variantQty + " Available";
                 document.querySelector("#product__variant-qty").style.display = "block";
 
               }
