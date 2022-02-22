6d81ef9f5396edf056e7279d0fdb879e2cdd9b36
Eric Clark <eclarrrk@users.noreply.github.com>
2022-02-22 16:50:40 -0500

    Trying it yet another way

--- assets/productinventory.js
+++ assets/productinventory.js
@@ -85,7 +85,8 @@ function checkavailablity(variation_value)
                 if(locationvar[locvariable]['qty'] < 10){document.querySelectorAll(".qtyerrorapi")[0].innerHTML=`<span class="qtynotice">Only ${locationvar[locvariable]['qty']} Left.</span>`;}
               	document.querySelectorAll(".product-form__row")[0].style.display = "block";
                 document.querySelectorAll(".alert--coming-soon")[0].style.display = "none";
-                document.querySelector("#product__variant-qty span").textContent += locationvar[locvariable]["qty"];
+                var variantQty = document.createTextNode(locationvar[locvariable]["qty"]);
+                document.querySelector("#product__variant-qty span").appendChild(variantQty);
                 document.querySelector("#product__variant-qty").style.display = "block";
 
               }
